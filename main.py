e365d151868f3d6a621d5f2889fba896fcaf8ff3
Jintheexplorer <jinwan.hyun@gmail.com>
2020-07-01 12:00:27 +0000

    port changed

--- main.py
+++ main.py
@@ -245,4 +245,4 @@ def gendershare_piechart(startDate, endDate, timeUnit, device, ages):
     }
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8080, debug=True)
+    app.run_server(host='0.0.0.0', port=80, debug=True)
